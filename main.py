a36cade5eb38584e65ceebcce0f9d72805fc5441
andrew-milos <day_7@ua.fm>
2021-03-27 19:30:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -57,8 +57,27 @@ logger = logging.getLogger("tg-bot-template")
 
 @app.route('/incoming', methods=['POST'])
 def incoming():
-	logger.info("received request. post data: {0}".format(request.get_data()))
+    logger.info("received request. post data: {0}".format(request.get_data()))
 	# handle the request here
+	    # every viber message is signed, you can verify the signature using this method
+    if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
+        return Response(status=403)
+
+    # this library supplies a simple way to receive a request object
+    viber_request = viber.parse_request(request.get_data())
+
+    if isinstance(viber_request, ViberMessageRequest):
+        message = viber_request.message
+        # lets echo back
+        viber.send_messages(viber_request.sender.id, [
+            message
+        ])
+    elif isinstance(viber_request, ViberSubscribedRequest):
+        viber.send_messages(viber_request.get_user.id, [
+            TextMessage(text="thanks for subscribing!")
+        ])
+    elif isinstance(viber_request, ViberFailedRequest):
+        logger.warn("client failed receiving message. failure: {0}".format(viber_request))
 	return "!", 200
 
 #@app.route('/', methods=['POST'])
