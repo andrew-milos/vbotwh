187cf2cc10ccfccba25ae2d67abdd342ce4ff68b
andrew-milos <day_7@ua.fm>
2021-03-27 18:25:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -118,8 +118,8 @@ def webhook_on():
 
     #bot.remove_webhook()
     #url = 'https://' + os.environ.get('HOST') + '/' + WEBHOOK_TOKEN
-    url = 'https://' + os.environ.get('HOST') + ':443/'
-    viber.set_webhook(url)
+    #url = 'https://' + os.environ.get('HOST') + ':443/'
+    viber.set_webhook('https://vbotwh.herokuapp.com:443/')
     #bot.set_webhook(url=url)
     logger.info(f'Webhook is ON! Url: %s', url)
     return "<h1>WebHook is ON!</h1>", 200
