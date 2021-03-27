abd373279acf58cf7cefc6d1cbf9358de4b867fa
andrew-milos <day_7@ua.fm>
2021-03-27 17:14:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ from flask import Flask, request
 
 import random
 
-#from viberbot import Api
-#from viberbot.api.bot_configuration import BotConfiguration
-#from viberbot.api.messages.text_message import TextMessage
-#from viberbot.api.viber_requests import ViberConversationStartedRequest
-#from viberbot.api.viber_requests import ViberFailedRequest
-#from viberbot.api.viber_requests import ViberMessageRequest
-#from viberbot.api.viber_requests import ViberSubscribedRequest
-#from viberbot.api.viber_requests import ViberUnsubscribedRequest
+from viberbot import Api
+from viberbot.api.bot_configuration import BotConfiguration
+from viberbot.api.messages.text_message import TextMessage
+from viberbot.api.viber_requests import ViberConversationStartedRequest
+from viberbot.api.viber_requests import ViberFailedRequest
+from viberbot.api.viber_requests import ViberMessageRequest
+from viberbot.api.viber_requests import ViberSubscribedRequest
+from viberbot.api.viber_requests import ViberUnsubscribedRequest
 
 
 #import sched
